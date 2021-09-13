b26c6f5e684ed65e50546bd830ee8f2a907dd000
David <davrui937@alu.centroafuera.es>
2021-09-13 14:24:51 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    var num=5465 //Esto es una variable
+    val cons = 12 //Esto es una constante
+    var boolean = true //Creo una variable booleana
+
+
+    println("Variable toma el valor de $num")
+    println("Variable toma el valor de $cons")
+
+    num = 54
+    println("Variable toma el valor de $num")
+
+
+    println("Variable toma el valor de $boolean")
+
+    boolean = false
+    println("Variable toma el valor de $boolean")
+}
